3bfb7abd9770ce0b27d3505f84d529bed3fc2326
heberzip <heber@ziptransfers.com>
2025-12-01 01:30:27 +0100

    feat: add bd configuration

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,7 +3,20 @@ import { Pool } from 'pg'
 
 import * as schema from './schema.ts'
 
+
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 })
+
 export const db = drizzle(pool, { schema })
